89db26ebd3418d48edb6cafb9d0b6c3f0128f011
bhagyashri <bchopkar@cemtrex.com>
2022-03-25 11:57:25 +0530

    Update main.js
    parrot and background changes

--- main.js
+++ main.js
@@ -20,16 +20,16 @@ camera.position.setZ(30);
 camera.position.setX(-3);
 
 renderer.render(scene, camera);
-     
+ 
 
 const loader = new GLTFLoader()
 const mixer = new THREE.AnimationMixer()
 loader.load(
   '/models/Parrot.glb',
     function (gltf) {
-      gltf.scene.scale.set(0.2,0.2,0.2)
-      gltf.scene.position.set(-15,10, 30);
-            
+      gltf.scene.scale.set(0.1,0.1,0.1)
+      gltf.scene.position.set(5,4,-20);
+     
       scene.add(gltf.scene)
     },
     (xhr) => {
@@ -43,11 +43,11 @@ loader.load(
 
 // Torus
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const torus = new THREE.Mesh(geometry, material);
+//const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+//const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+//const torus = new THREE.Mesh(geometry, material);
 
-scene.add(torus);
+//scene.add(torus);
 
 // Lights
 
@@ -82,16 +82,16 @@ Array(200).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('/images/space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('/images/beachPhoto.jpg');
 scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load('/images/beachPhoto.jpg');
+//const jeffTexture = new THREE.TextureLoader().load('/images/beachPhoto.jpg');
 
-const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
+//const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
 
-scene.add(jeff);
+//scene.add(jeff);
 
 // Moon
 
@@ -108,11 +108,12 @@ const moon = new THREE.Mesh(
 
 scene.add(moon);
 
-moon.position.z = 30;
-moon.position.setX(-10);
+moon.position.z = 10;
+moon.position.y = 40;
+moon.position.setX(-100);
 
-jeff.position.z = -5;
-jeff.position.x = 2;
+//jeff.position.z = -5;
+//jeff.position.x = 2;
 
 // Scroll Animation
 
@@ -122,12 +123,13 @@ function moveCamera() {
   moon.rotation.y += 0.075;
   moon.rotation.z += 0.05;
 
-  jeff.rotation.y += 0.01;
-  jeff.rotation.z += 0.01;
+  //jeff.rotation.y += 0.01;
+  //jeff.rotation.z += 0.01;
 
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
   camera.rotation.y = t * -0.0002;
+  
 }
 
 document.body.onscroll = moveCamera;
@@ -138,9 +140,9 @@ moveCamera();
 function animate() {
   requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
+  //torus.rotation.x += 0.01;
+  //torus.rotation.y += 0.005;
+  //torus.rotation.z += 0.01;
 
   moon.rotation.x += 0.005;
 
